af80aeffe4e8147640cf500ea61aec9109519b0f
user <yenje7@naver.com>
2022-05-23 23:46:44 +0900

    controller 분리

--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -3,22 +3,23 @@ import is from '@sindresorhus/is';
 // 폴더에서 import하면, 자동으로 폴더의 index.js에서 가져옴
 import { loginRequired } from '../middlewares';
 import { userService } from '../services';
-import { register,login,getUserlist,editUserData } from '../controller';
+import * as usercontroller from '../controller';
+// import { register,login,getUserlist,editUserData } from '../controller';
 
 const userRouter = Router();
 
 // 회원가입 api (아래는 /register이지만, 실제로는 /api/register로 요청해야 함.)
-userRouter.post('/register',register);
+userRouter.post('/register',usercontroller.register);
 
 // 로그인 api (아래는 /login 이지만, 실제로는 /api/login로 요청해야 함.)
-userRouter.post('/login', login);
+userRouter.post('/login', usercontroller.login);
 
 // 전체 유저 목록을 가져옴 (배열 형태임)
 // 미들웨어로 loginRequired 를 썼음 (이로써, jwt 토큰이 없으면 사용 불가한 라우팅이 됨)
-userRouter.get('/userlist', loginRequired, getUserlist);
+userRouter.get('/userlist', loginRequired, usercontroller.getUserlist);
 
 // 사용자 정보 수정
 // (예를 들어 /api/users/abc12345 로 요청하면 req.params.userId는 'abc12345' 문자열로 됨)
-userRouter.patch('/users/:userId',loginRequired,editUserData);
+userRouter.patch('/users/:userId',loginRequired,usercontroller.editUserData);
 
 export { userRouter };
