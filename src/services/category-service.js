d818ee56e9af026bf9f41995e66869ce7b056182
xfrnk2 <xfrnk2@gmail.com>
2022-05-23 22:49:15 +0900

    feat: add creating category api

--- src/services/category-service.js
+++ src/services/category-service.js
@@ -9,3 +9,24 @@ exports.getCategory = async (categoryName) => {
     throw new Error(err);
   }
 };
+
+exports.insertCategory = async (name) => {
+  try {
+    console.log(name);
+    if (!name) {
+      throw new Error("유효한 값을 입력하세요.");
+    }
+    if (this.getCategory(name)) {
+      throw new Error("중복된 이름입니다.");
+    }
+
+    const category = new Category({
+      name: name,
+    });
+    const result = await category.save();
+    return result;
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
