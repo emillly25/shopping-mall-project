128e44072bb81c79b8ff10b2e9a058a3f710a1cd
xfrnk2 <xfrnk2@gmail.com>
2022-05-24 22:26:09 +0900

    refactor: modify category-get method to optional uri

--- src/routers/category-router.js
+++ src/routers/category-router.js
@@ -3,7 +3,7 @@ import { categoryController } from "../controllers/category-controller.js";
 const categoryRouter = Router();
 
 console.log(categoryRouter);
-categoryRouter.get("/:categoryName", categoryController.getCategory);
+categoryRouter.get("/:categoryName*?", categoryController.getCategory);
 categoryRouter.post("/", categoryController.insertCategory);
 categoryRouter.patch("/", categoryController.updateCategory);
 categoryRouter.delete("/", categoryController.deleteCategory);
