ac4b2afd5ce63fcc0d1f470c2836a0116b83e34a
sehee <seeeh0128@gmail.com>
2022-05-25 11:36:50 +0900

    login 반응형과 css코드 리팩토링

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -6,7 +6,18 @@ const emailCheck = document.querySelector('#emailCheck');
 const passwordIcon = document.querySelector('#passwordIcon');
 
 
-if(emailInput.value){
-    console.log('ㅇ');
-}
+//로그인 input value 검증
+submitBtn.addEventListener('click',function(e){
+    e.preventDefault();
+    const email = emailInput.value;
+    const password = passwordInput.value;
 
+    let reg = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+    if(reg.test(email) === false){
+        console.log('이메일이 유효하지 않습니다.')
+    }else if(password.length <5){
+        console.log('비밀번호가 틀렸습니다.')
+    }else{
+        console.log('로그인 성공')
+    }
+})
