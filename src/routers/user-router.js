616514248cd4e9f6363fa49696a9732c92aca569
user <yenje7@naver.com>
2022-05-26 20:16:12 +0900

    관리자 미들웨어

--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 // 폴더에서 import하면, 자동으로 폴더의 index.js에서 가져옴
-import { errorHandler, loginRequired,registerRequired } from '../middlewares';
+import { errorHandler, loginRequired,registerRequired,adminRequired } from '../middlewares';
 import * as usercontroller from '../controllers';
 // import { register,login,getUserlist,editUserData } from '../controller';
 
@@ -13,8 +13,8 @@ userRouter.post('/register',registerRequired,usercontroller.register);
 userRouter.post('/login', usercontroller.login);
 
 // 전체 유저 목록을 가져옴 (배열 형태임)
-// 미들웨어로 loginRequired 를 썼음 (이로써, jwt 토큰이 없으면 사용 불가한 라우팅이 됨)
-userRouter.get('/userlist', loginRequired, usercontroller.getUserlist);
+// 미들웨어로 adminRequired 를 썼음 (이로써, jwt 토큰이 없으면 사용 불가한 라우팅이 됨)
+userRouter.get('/userlist',adminRequired, usercontroller.getUserlist);
 
 // 사용자 정보 수정
 // (예를 들어 /api/users/abc12345 로 요청하면 req.params.userId는 'abc12345' 문자열로 됨)
