9895a19825a361a17959d6625f43a85de1297b72
sehee <seeeh0128@gmail.com>
2022-05-26 17:13:47 +0900

    주문완료 레이아웃 구성

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -11,6 +11,8 @@ viewsRouter.use('/register', serveStatic('register'));
 viewsRouter.use('/login', serveStatic('login'));
 viewsRouter.use('/booklist',serveStatic('booklist'));
 viewsRouter.use('/bookdetail',serveStatic('bookdetail'));
+viewsRouter.use('/order',serveStatic('order'));
+viewsRouter.use('/order-complete',serveStatic('order-complete'));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
