4637729f3f2a569198db526ca9e0cebc1ecee956
sehee <seeeh0128@gmail.com>
2022-05-26 09:59:59 +0900

    bookdetail 링크 연결

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -10,6 +10,7 @@ viewsRouter.use('/', serveStatic('home'));
 viewsRouter.use('/register', serveStatic('register'));
 viewsRouter.use('/login', serveStatic('login'));
 viewsRouter.use('/booklist',serveStatic('booklist'));
+viewsRouter.use('/bookdetail',serveStatic('bookdetail'));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
