2c18a3ee53e93d1b4287fc79940c7b6a38463846
shinbian11 <shinbian11@gmail.com>
2022-06-02 00:32:29 +0900

    회원정보 페이지에도 template.js, css 적용

--- src/views/home/template.js
+++ src/views/home/template.js
@@ -171,31 +171,32 @@ export const insertHeader = () => {
 // scroll버튼
 const body = document.querySelector('body');
 const scroll_btn = document.querySelector('.scroll_btn');
+if (scroll_btn) {
+  scroll_btn.innerHTML = `
+    <div id="top_btn"><img src="./img/top_btn.png" alt="top_btn" /></div>
+    <div id="bottom_btn"><img src="./img/bottom_btn.png" alt="bottom_btn" /></div>
+    `;
 
-scroll_btn.innerHTML = `
-<div id="top_btn"><img src="../img/top_btn.png" alt="top_btn" /></div>
-<div id="bottom_btn"><img src="../img/bottom_btn.png" alt="bottom_btn" /></div>
-`;
+  window.addEventListener('scroll', function () {
+    let y = window.pageYOffset;
+    if (y > 0) {
+      scroll_btn.style.display = 'block';
+    } else {
+      scroll_btn.style.display = 'none';
+    }
+  });
 
-window.addEventListener('scroll', function () {
-  let y = window.pageYOffset;
-  if (y > 0) {
-    scroll_btn.style.display = 'block';
-  } else {
-    scroll_btn.style.display = 'none';
-  }
-});
+  const top_btn = document.querySelector('#top_btn');
+  const bottom_btn = document.querySelector('#bottom_btn');
 
-const top_btn = document.querySelector('#top_btn');
-const bottom_btn = document.querySelector('#bottom_btn');
-
-top_btn.addEventListener('click', function () {
-  scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-});
-bottom_btn.addEventListener('click', function () {
-  const endY = body.offsetHeight;
-  scrollTo({ top: endY, left: 0, behavior: 'smooth' });
-});
+  top_btn.addEventListener('click', function () {
+    scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  });
+  bottom_btn.addEventListener('click', function () {
+    const endY = body.offsetHeight;
+    scrollTo({ top: endY, left: 0, behavior: 'smooth' });
+  });
+}
 
 // footer.js
 
