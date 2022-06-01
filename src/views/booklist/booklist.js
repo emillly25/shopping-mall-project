9b5674a851b342c750a010c8ce5667f30c743499
sehee <seeeh0128@gmail.com>
2022-06-01 13:18:22 +0900

    제품 리스트에서 localStorage에 수량 담아서 보내기, 수량조절 버튼 추가

--- src/views/booklist/booklist.js
+++ src/views/booklist/booklist.js
@@ -1,40 +1,81 @@
 import * as Api from '/api.js';
-
-
 const items = document.querySelector('.items');
-const bookName = document.querySelector('.bookName span'); //책제목 
-const bookAuthor = document.querySelector('.bookAuthor span');  //저자
-const bookPrice = document.querySelector('.bookPrice span');  //판매가격
-const bookInformation = document.querySelector('.bookInformation span'); //책설명
-const bookImg = document.querySelector('.bookImg img');  //책사진
 
+async function booksRendering(){
+    //DB에 있는 전체 책 정보 가져오기
+    const getBookInfo = async function(){
+        const res = await Api.get('/api/product')
+        const data = res.result
+        console.log('받아온 전체 데이터',data)
+        data.map(({_id, name, price, author,information, imgUrl })=>{
+            const htmlInfo = `<div class="bookList">
+            <div class="bookImg" id="url"><a href="/bookdetail/?productId=${_id}"><img src="${imgUrl}"></a></div>
+            <div class="bookInfo"> 
+                <div class="bookName"><span><a href="/bookdetail/?productId=${_id}">${name}</a></span></div>
+                <div class="bookAuthor"><span>${author}</span></div>
+                <div class="qty">
+                <input data-id="${_id}" class="minus" type="button" value="-">
+                <input data-id="${_id}" class="num" type="number" value="1" min="1" step="1" style="width: 30px; height:30px; text-align:center;">
+                <input data-id="${_id}" class="plus" type="button" value="+">
+            </div>
+                <div class="bookPrice"><span id="showPrice" data-id="${_id}">${price}</span></div>
+                <div class=" bookInformation"><span>${information}</span></div>
+            </div>
+        
+            <div class="bookBuy">
+                <div class="buyBtn"><input id="buyBtn" type="button" value="구매하기" onClick="location.href='/order'"></div>
+                <div class="cartBtn"><input data-id="${_id}" id="cartBtn" type="button" value="장바구니"></div>
+            </div></div>` 
+            items.innerHTML += htmlInfo 
+        })
 
-//json에서 데이터 받아오는 함수
-const getBookInfo = async function(){
-    const res = await Api.get('/api/product')
-    const data = res.result
-    console.log('받아온 전체 데이터',data)
-    data.map(({_id, name, price, author,information, imgUrl })=>{
-        const htmlInfo = `<div class="bookList">
-        <div class="bookImg" id="url"><a href="/bookdetail/?productId=${_id}"><img src="${imgUrl}"></a></div>
-        <div class="bookInfo"> 
-            <div class="bookName"><span><a href="/bookdetail/?productId=${_id}">${name}</a></span></div>
-            <div class="bookAuthor"><span>${author}</span></div>
-            <div class="bookPrice"><span id="showPrice" data-id="${_id}">${price}</span></div>
-            <div class=" bookInformation"><span>${information}</span></div>
-        </div>
+    }
+    await getBookInfo()
+
+    // 책 정보 element
+    const bookName = document.querySelector('.bookName span'); //책제목 
+    const bookAuthor = document.querySelector('.bookAuthor span');  //저자
+    const bookPrice = document.querySelectorAll('.bookPrice span');  //판매가격
+    const bookInformation = document.querySelector('.bookInformation span'); //책설명
+    const bookImg = document.querySelector('.bookImg img');  //책사진
+        
+    //수량 조절버튼 element 
+    const minus = document.querySelectorAll('.minus');  //minus Btn
+    const plus =  document.querySelectorAll('.plus');   //plus Btn
+    const num =  document.querySelectorAll('.num');  //input(number)
+
+    // NodeList to Arr
+    const numArr = Array.prototype.slice.call(num); 
+    const bookPriceArr = Array.prototype.slice.call(bookPrice); 
+
     
-        <div class="bookBuy">
-            <div class="buyBtn"><input id="buyBtn" type="button" value="구매하기" onClick="location.href='/order'"></div>
-            <div class="cartBtn"><input data-id="${_id}" id="cartBtn" type="button" value="장바구니"></div>
-        </div></div>` 
-        items.innerHTML += htmlInfo 
-     
+    async function handleUpdateQuantity(e) {
+        const res = await Api.get('/api/product')
+        const data = res.result
+        if (e.target.classList.contains('minus')){
+            const id = e.target.dataset.id
+            const num = numArr.find(el=> el.dataset.id === id)
+            const firstPrice = data.find(el=> el._id === id).price
+            const price = bookPriceArr.find(el=> el.dataset.id === id)
+            num.stepDown()
+            price.innerText = num.value * firstPrice
+        } else {
+            const id = e.target.dataset.id
+            const num = numArr.find(el=> el.dataset.id === id)
+            const firstPrice = data.find(el=> el._id === id).price
+            const price = bookPriceArr.find(el=>el.dataset.id === id)
+            num.stepUp()
+            price.innerText = Number(num.value) * firstPrice
+        }
+    }
     
-    })
+    plus.forEach(el=> el.addEventListener('click', handleUpdateQuantity))
+    minus.forEach(el=> el.addEventListener('click', handleUpdateQuantity))
+    
+
+    // 장바구니에 상품 담기
     const cartBtn = document.querySelectorAll('#cartBtn')
     const saveBooks = []
-
     async function addToCart(){
         cartBtn.forEach(el=>{
             const showPrice = document.querySelectorAll('#showPrice')
@@ -44,22 +85,21 @@ const getBookInfo = async function(){
                 if(localCart === null){
                     const id = e.target.dataset.id
                     const price = showPriceArr.find(el=> el.dataset.id === id)
+                    const num = numArr.find(el=> el.dataset.id === id)
                     const obj = {};
                     obj._id = id
-                    obj.quantity = 1
+                    obj.quantity = num.value
                     obj.price = Number(price.textContent)
                     saveBooks.push(obj)
                     window.localStorage.setItem('productId', JSON.stringify(saveBooks))
                     moveCart()
                 }else{
-                     // 일단 중복이고 뭐고 다 배열에 넣어서 localStorage에 저장해
-                    // 다시 localStorage조회해서 중복 제거해
-                    //그리고 안내창 띄워 그럼 끝.
                     const id = e.target.dataset.id
                     const price = showPriceArr.find(el=> el.dataset.id === id)
+                    const num = numArr.find(el=> el.dataset.id === id)
                     const obj = {};
                     obj._id = id
-                    obj.quantity = 1
+                    obj.quantity = num.value
                     obj.price = Number(price.textContent)
                     saveBooks.push(obj)
                     const newBooks = saveBooks.concat(JSON.parse(localCart));
@@ -77,17 +117,22 @@ const getBookInfo = async function(){
     }
     addToCart()
 
-
-}
-
-getBookInfo()
-
-function moveCart(){
-    if(confirm(`장바구니에 추가되었습니다. 장바구니로 이동하시겠습니까? `)){
-        location.href = '/cart' 
+    function moveCart(){
+        if(confirm(`장바구니에 추가되었습니다. 장바구니로 이동하시겠습니까? `)){
+            location.href = '/cart' 
+        }
+        
     }
     
+
 }
+
+
+booksRendering()
+
+
+
+
 
 
 
