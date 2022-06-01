8563c16456fccd4af3153d954952e4467f8ee897
shinbian11 <shinbian11@gmail.com>
2022-06-01 23:30:09 +0900

    footer와 scrollbtn 부분도 뿌려주는 걸로 교체

--- src/views/home/header.js
+++ src/views/home/header.js
@@ -65,28 +65,6 @@ window.addEventListener('scroll', function () {
   }
 });
 
-// scroll버튼
-const body = document.querySelector('body');
-const scroll_btn = document.querySelector('.scroll_btn');
-const top_btn = document.querySelector('#top_btn');
-const bottom_btn = document.querySelector('#bottom_btn');
-
-window.addEventListener('scroll', function () {
-  let y = window.pageYOffset;
-  if (y > 0) {
-    scroll_btn.style.display = 'block';
-    top_btn.addEventListener('click', function () {
-      scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-    });
-    bottom_btn.addEventListener('click', function () {
-      const endY = body.offsetHeight;
-      scrollTo({ top: endY, left: 0, behavior: 'smooth' });
-    });
-  } else {
-    scroll_btn.style.display = 'none';
-  }
-});
-
 //dropdown menu
 const dropmenu = document.querySelector('#dropmenu');
 const dropdown = document.querySelector('.dropdown');
